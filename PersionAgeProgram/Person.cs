68f7880fd6a748692ba77e78a14d67f7570a412c
Subhash Barela <barelasubhash16@gmail.com>
2023-07-11 15:07:23 +0530

    [subhash] handle exception in case of null value

--- PersionAgeProgram/Person.cs
+++ PersionAgeProgram/Person.cs
@@ -8,27 +8,37 @@ namespace PersionAgeProgram
 {
     public class Person
     {
+        
         public void ValidAge(int age)
         {
-            if (age >= 1 && age <= 14)
-            {
-                Console.WriteLine("Children ");
+           
+
+                if (age <= 0)
+                {
+                throw new PersonException("Please provide valid age");
+                }
+            try{ 
+                    if (age >= 1 && age <= 14)
+                    {
+                        Console.WriteLine("Children ");
+                    }
+                    else if (age >= 15 && age <= 24)
+                    {
+                        Console.WriteLine("Youth person");
+                    }
+                    else if (age > 24 && age <= 64)
+                    {
+                        Console.WriteLine("Adults person");
+                    }
+                    else if (age > 64 && age < 130)
+                    {
+                        Console.WriteLine("Seniors person");
+                    }
+                
             }
-            else if (age >= 15 && age <= 24)
+            catch (ArgumentNullException ex)
             {
-                Console.WriteLine("Youth person");
-            }
-            else if (age > 24 && age <= 64)
-            {
-                Console.WriteLine("Adults person");
-            }
-            else if (age > 64 && age < 130)
-            {
-                Console.WriteLine("Seniors person");
-            }
-            else
-            {
-                Console.WriteLine("Please insert valid age");
+                throw new PersonException(ex.Message);
             }
         }
     }
